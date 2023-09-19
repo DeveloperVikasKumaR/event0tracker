93e5d2caf47ce9e0e6a87595961b2f8243af39b6
VIKAS KR <developer.vikas.kumar@gmail.com>
2023-09-19 12:23:06 +0530

    feat : - First commit with header and footer block

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Route,Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+import EventDetail from "./pages/EventDetail"
+import MainLayout from './layout/MainLayout'
+
+const App = () => {
+  return (
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/event/:eventId' element={<EventDetail />} />
+        </Route>
+      </Routes>
+  )
+}
+
+export default App

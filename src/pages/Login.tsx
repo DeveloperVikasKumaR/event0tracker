71738ededf4bc4d423716f285b71b1f6062e30e2
VIKAS KR <developer.vikas.kumar@gmail.com>
2023-09-21 23:47:08 +0530

    refactor : added empty component container.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
+import Container from "../components/atom/Container"
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <Container>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem a molestias rerum corrupti accusamus ex dicta suscipit quibusdam, veritatis sequi in consectetur labore, adipisci architecto ab mollitia doloremque hic eligendi? 
+    </Container>
   )
 }
 
